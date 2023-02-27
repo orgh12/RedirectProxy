5e7b4f636536e9eb27bffc00ce61f2a82e27f9b5
orgh12 <or.shenbahar@gmail.com>
2023-02-27 16:23:13 +0200

    got it working
    prob final version

--- test.go
+++ test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"crypto/rsa"
 	"crypto/tls"
 	"crypto/x509"
@@ -10,12 +9,10 @@ import (
 	"github.com/AdguardTeam/gomitmproxy"
 	"github.com/AdguardTeam/gomitmproxy/mitm"
 	"github.com/AdguardTeam/gomitmproxy/proxyutil"
-	"io/ioutil"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 	"time"
 )
@@ -38,7 +35,7 @@ func (c *CustomCertsStorage) Set(key string, cert *tls.Certificate) {
 
 func main() {
 	// Read the MITM cert and key.
-	tlsCert, err := tls.LoadX509KeyPair("C:\\Users\\orshe\\GolandProjects\\demo.crt", "C:\\Users\\orshe\\GolandProjects\\demo.key")
+	tlsCert, err := tls.LoadX509KeyPair("demo.crt", "demo.key")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -62,44 +59,20 @@ func main() {
 	// Set certs organization.
 	mitmConfig.SetOrganization("gomitmproxy")
 
-	// Generate a cert-key pair for the HTTP-over-TLS proxy.
-	proxyCert, err := mitmConfig.GetOrCreateCert("127.0.0.1")
-	if err != nil {
-		panic(err)
-	}
-	tlsConfig := &tls.Config{
-		Certificates: []tls.Certificate{*proxyCert},
-	}
-
 	proxy := gomitmproxy.NewProxy(gomitmproxy.Config{
 		ListenAddr: &net.TCPAddr{
 			IP:   net.IPv4(0, 0, 0, 0),
 			Port: 8080,
 		},
-		TLSConfig: tlsConfig,
+		MITMConfig: mitmConfig,
 		OnRequest: func(session *gomitmproxy.Session) (request *http.Request, response *http.Response) {
 			req := session.Request()
 
 			log.Printf("onRequest: %s %s", req.Method, req.URL.String())
 			fmt.Println(req.URL.Host)
-			if req.URL.Host == "www.chess.com:443" {
-				//body := strings.NewReader("<html><body><h1>Replaced response</h1></body></html>")
-				//res := proxyutil.NewResponse(http.StatusOK, body, req)
-				//res.Header.Set("Content-Type", "text/html")
-				//
-				//// Use session props to pass the information about request being blocked
-				//session.SetProp("blocked", true)
-				resp, err := http.Get("https://www.facebook.com/")
-				if err != nil {
-					log.Fatal(err)
-				}
-				res := proxyutil.NewResponse(http.StatusMovedPermanently, resp.Body, nil)
-				res.Header.Set("Content-Type", "text/html")
-				res.Header.Set("Location", "https://www.facebook.com")
+			if req.URL.Host == "www.chess.com" {
 				session.SetProp("blocked", true)
-				return nil, res
 			}
-
 			return nil, nil
 		},
 		OnResponse: func(session *gomitmproxy.Session) *http.Response {
@@ -107,43 +80,21 @@ func main() {
 
 			if _, ok := session.GetProp("blocked"); ok {
 				log.Printf("onResponse: was blocked")
+				redirectURL := "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+				resp, err := http.Get(redirectURL)
+				if err != nil {
+					log.Fatal(err)
+				}
+				res := proxyutil.NewResponse(http.StatusFound, resp.Body, nil)
+				res.Header.Set("Content-Type", "text/html")
+				res.Header.Set("Location", redirectURL)
+				return res
 			}
 
-			res := session.Response()
-			req := session.Request()
-
-			if strings.Index(res.Header.Get("Content-Type"), "text/html") != 0 {
-				// Do nothing with non-HTML responses
-				return nil
-			}
-
-			b, err := proxyutil.ReadDecompressedBody(res)
-			// Close the original body
-			_ = res.Body.Close()
-			if err != nil {
-				return proxyutil.NewErrorResponse(req, err)
-			}
-
-			// Use latin1 before modifying the body
-			// Using this 1-byte encoding will let us preserve all original characters
-			// regardless of what exactly is the encoding
-			body, err := proxyutil.DecodeLatin1(bytes.NewReader(b))
-			if err != nil {
-				return proxyutil.NewErrorResponse(session.Request(), err)
-			}
-
-			// Modifying the original body
-			modifiedBody, err := proxyutil.EncodeLatin1(body + "<!-- EDITED -->")
-			if err != nil {
-				return proxyutil.NewErrorResponse(session.Request(), err)
-			}
-
-			res.Body = ioutil.NopCloser(bytes.NewReader(modifiedBody))
-			res.Header.Del("Content-Encoding")
-			res.ContentLength = int64(len(modifiedBody))
-			return res
+			return session.Response()
 		},
 	})
+
 	err = proxy.Start()
 	if err != nil {
 		log.Fatal(err)
